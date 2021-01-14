4e47b596afa99488391301dadd8e20c1e0f2ebad
Roee Shachar <shacharroy14@gmail.com>
2021-01-14 17:57:37 +0200

    fix: not working with regular user, improve configurations (#11)
    * fix: not working with regular user, improve configurations
    
    * fix: remove vrt file

--- src/worker.py
+++ src/worker.py
@@ -4,6 +4,7 @@ from logger.jsonLogger import Logger
 from config import read_json
 from gdal2tiles import generate_tiles
 import requests
+import shutil
 
 
 class Worker:
@@ -13,38 +14,60 @@ class Worker:
                                 '../config/production.json')
         self.__config = read_json(config_path)
 
+    def vrt_file_location(self, discrete_id):
+        output_folder_name = self.__config["gdal"]["vrt"]["folder"]
+        output_file_name = '{0}.vrt'.format(discrete_id)
+        output_path = path.join(output_folder_name, output_file_name) 
+        return output_path
+
+    def remove_temp_files(self):
+        bucket = self.__config["s3"]["bucket"]
+        s3_local_path = '/vsis3/{0}'.format(bucket)
+        self.log.info('Removing folder {0}'.format(s3_local_path))
+        shutil.rmtree(s3_local_path)
+
+
     def set_gdal_s3(self):
         gdal.SetConfigOption('AWS_ACCESS_KEY_ID',
-                                self.__config["s3"]["access_key_id"])
+                             self.__config["s3"]["access_key_id"])
         gdal.SetConfigOption('AWS_SECRET_ACCESS_KEY',
-                                self.__config["s3"]["secret_access_key"])
+                             self.__config["s3"]["secret_access_key"])
         gdal.SetConfigOption(
             'AWS_S3_ENDPOINT', self.__config["s3"]["endpoint_url"])
         gdal.SetConfigOption('AWS_HTTPS', self.__config["s3"]["ssl_enabled"])
         gdal.SetConfigOption('AWS_VIRTUAL_HOSTING',
-                                self.__config["s3"]["virtual_hosting"])
+                             self.__config["s3"]["virtual_hosting"])
         gdal.SetConfigOption('CPL_VSIL_USE_TEMP_FILE_FOR_RANDOM_WRITE', 'YES')
 
     def buildvrt_utility(self, task_values):
-        discrete_id = task_values["discrete_id"]
-        self.log.info("Starting process GDAL-BUILD-VRT on ID: {0}".format(discrete_id))
-        get_discrete_by_id_url = f'{self.__config["discrete_storage"]["url"]}/{discrete_id}'
+        key = task_values["discrete_id"]
+        get_discrete_by_id_url = '{0}/{1}'.format(self.__config["discrete_storage"]["url"], key)
         discrete_layer = requests.get(url=get_discrete_by_id_url).json()
 
         vrt_config = {
-            'VRTNodata': '0',
-            'outputSRS': 'EPSG:4326',
-            'resampleAlg': 'bilinear'
+            'VRTNodata': self.__config["gdal"]["vrt"]["no_data"],
+            'outputSRS': self.__config["gdal"]["vrt"]["output_srs"],
+            'resampleAlg': self.__config["gdal"]["vrt"]["resample_algo"]
         }
-        gdal.BuildVRT(f'{discrete_id}.vrt',
-                      discrete_layer["tiffs"], **vrt_config)
+
+        self.log.info("Starting process GDAL-BUILD-VRT on ID: {0}".format(key))
+        gdal.BuildVRT(self.vrt_file_location(key), discrete_layer["tiffs"], **vrt_config)
+
 
     def gdal2tiles_utility(self, task_values):
-        zoom_levels = task_values["zoom_levels"]  
+        self.set_gdal_s3()
+        zoom_levels = task_values["zoom_levels"]
         key = task_values["discrete_id"]
-        self.log.info("Starting process GDAL2TILES on ID: {0}".format(key))
+
+        options = {
+            'resampling': 'bilinear',
+            'tmscompatible': True,
+            'profile': 'geodetic',
+            'zoom': '{0}-{1}'.format(zoom_levels[0], zoom_levels[len(zoom_levels)-1])
+        }
 
         bucket = self.__config["s3"]["bucket"]
-        generate_tiles(f'{key}.vrt', f'/vsis3/{bucket}',
-                                zoom=f'{zoom_levels[0]}-{zoom_levels[len(zoom_levels)-1]}',
-                                resampling='bilinear', tmscompatible=True, profile='geodetic')
+        s3_path = '/vsis3/{0}/{1}'.format(bucket, key)
+
+        self.log.info("Starting process GDAL2TILES on ID: {0}".format(key))
+        generate_tiles(self.vrt_file_location(key), s3_path, **options)
