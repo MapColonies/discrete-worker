20086cedb014cbbe6f330aec5046b2a26b280231
Anguiar <shacharroy14@gmail.com>
2021-01-19 10:33:01 +0200

    fix: tiles_outputs_folder created only in filesystem mode

--- src/main.py
+++ src/main.py
@@ -24,11 +24,14 @@ class Main:
 
     def _start_service(self):
         self.log.info('Service is listening to broker: {0}, topic: {1}'.format(self.__config["kafka"]["host_ip"], self.__config["kafka"]["topic"]))
+        storage_provider = self.__config['storage_provider'].upper()
         try:
             self.__utilities.create_folder(worker_constants.VRT_OUTPUT_FOLDER_NAME)
-            self.__utilities.create_folder(worker_constants.TILES_OUTPUT_FOLDER_NAME)
 
-            if (self.__config['storage_provider'].upper() == StorageProvider.S3):
+            if (storage_provider == StorageProvider.FS):
+                self.__utilities.create_folder(worker_constants.TILES_OUTPUT_FOLDER_NAME)
+
+            if (storage_provider == StorageProvider.S3):
                 Utilities.set_gdal_s3()
 
             self.__task_handler.handle_tasks()
